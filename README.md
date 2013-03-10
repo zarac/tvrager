904d42cb7b1a97006da0317f60c1bf7cc20b16d1
Hannes Landstedt <github.com@zarac.se>
2013-03-10 10:30:44 +0100

    Three output formats (xml, xml2js and cleaned) are supported.

--- README.md
+++ README.md
@@ -1,20 +1,66 @@
 # tvrager
 A Node.js client library for [TVRage](http://www.tvrage.com)'s API.
 
+
 ## Installation
     npm install tvrager
 
+
 ## Usage
-See [test.js](test.js).
+Example of searching for episodes matching "Person of Interest".
+    tvrager.find('Person of Interest', function(list) {
+        console.log(list);
+    });
+
+Example of getting an episode list for "Person of Interest".
+    tvrager.getEpisodeList('28376', function(list) {
+        console.log(list);
+    });
+
+The two examples above use the default, cleaned, output format. All functions
+can take an optional parameter (at the end) to specify the output format as
+either 'xml' or 'xml2js'. 'xml' is simply what tvrage returns (an XML string),
+while 'xml2js' is that XML made into an object using [xml2js][].
+
+Example of getting show information, for "Person of Interest", in XML (string)
+format.
+    tvrager.getShowInfo('28376', function(list) {
+        console.log(list);
+    }, 'xml');
+
+Example of getting a list, in 'xml2js' format, of all shows. 
+    tvrager.getListShows(function(list) {
+        console.log(list);
+    }, 'xml2js');
+
+__NOTE__: _The "cleaned" format might miss out on some information. However,
+the functions mapping the data 'xml2js' to the cleaned format can easily be
+modified._
+
+See [test.js](test.js) for more examples.
+
+
+## TODO
+* Implement the cleaners for find, findFull, getEpisodeInfo, getFull,
+  getShowInfo and listShows. At the moment, they're simply the same as
+  the 'xml2js' format.
+* Proper testing.
+
 
 ## Source
 https://github.com/zarac/tvrager
 
+
 ## Author
 Hannes Landstedt a.k.a. zarac
+
 
 ## License
 NULL (No Unnecessary License - License)
 
+
 ## Extra Keywords
-node api module
+node module
+
+
+[xml2js]: https://github.com/Leonidas-from-XIV/node-xml2js 
--- main.js
+++ main.js
@@ -27,50 +27,148 @@ var http = require('http'),
 
 var baseUrl = 'http://services.tvrage.com/feeds/';
 
-var ragify = function(url, callback) {
+var ragify = function(url, callback, format) {
     http.get(url, function(respons) {
-        var html = '';
+        var xml = '';
         respons.on('data', function(chunk) {
-            html = html + chunk;
+            xml = xml + chunk;
         });
         respons.on('end', function() {
-            xml2js.parseString(html, function(err, result) {
-                if (err) callback({ error: err });
-                else callback(result);
-            });
+            if (format === 'xml') callback( { xml: xml });
+            else jsonify(xml, callback);
         });
     }).on('error', function(e) {
         callback({ error: e.message });
     });
 };
 
-module.exports = {
-    find : function(name, callback) {
-        ragify(baseUrl + 'search.php?show=' + name, callback);
-    },
+var jsonify = function(xml, callback) {
+    xml2js.parseString(xml, function(err, result) {
+        if (err) callback({ error: err });
+        else callback(result);
+    });
+};
 
-    findFull : function(name, callback) {
-        ragify(baseUrl + 'full_search.php?show=' + name, callback);
-    },
 
-    getEpisodeInfo : function(sid, ep, callback) {
-        ragify(baseUrl + 'episodeinfo.php?sid=' + sid + '&ep=' + ep, callback);
-    },
+//* Cleaners
+var clean_find = function(xml2js) {
+    /// TODO
+    return xml2js;
+};
 
-    getEpisodeList : function(id, callback) {
-        ragify(baseUrl + 'episode_list.php?sid=' + id, callback);
-    },
+var clean_find_full = function(xml2js) {
+    /// TODO
+    return xml2js;
+};
 
-    getFull : function(id, callback) {
-        ragify(baseUrl + 'full_show_info.php?sid=' + id, callback);
-    },
+var clean_episode_info = function(xml2js) {
+    /// TODO
+    return xml2js;
+};
 
-    getShowInfo : function(id, callback) {
-        ragify(baseUrl + 'showinfo.php?sid=' + id, callback);
-    },
-
-    listShows : function(callback) {
-        ragify(baseUrl + 'show_list.php', callback);
+var clean_episode_list = function(list) {
+    /// TODO
+    var out = {
+        name: list.Show.name[0],
+        seasons: []
+    };
+    for (var i = 0; i < list.Show.Episodelist[0].Season.length; i++) {
+        var s = list.Show.Episodelist[0].Season[i];
+        var season = {
+            number: s.$.no,
+            episodes: s.episode
+        }
+        out.seasons.push(season);
     }
+    return out;
+};
 
+var clean_full = function(xml2js) {
+    /// TODO
+    return xml2js;
+};
+
+var clean_show_info = function(xml2js) {
+    /// TODO
+    return xml2js;
+};
+
+var clean_list_shows = function(xml2js) {
+    /// TODO
+    return xml2js;
+};
+
+
+//* Exports
+module.exports = {
+    find : function(name, callback, format) {
+        if (!format || !(format == 'xml' || format == 'xml2js')) {
+            var realcallback = callback;
+            callback = function(result) {
+                realcallback(clean_find(result));
+            }
+        }
+        ragify(baseUrl + 'search.php?show=' + name, callback, format);
+    },
+
+    findFull : function(name, callback, format) {
+        if (!format || !(format == 'xml' || format == 'xml2js')) {
+            var realcallback = callback;
+            callback = function(result) {
+                realcallback(clean_find_full(result));
+            }
+        }
+        ragify(baseUrl + 'full_search.php?show=' + name, callback, format);
+    },
+
+    getEpisodeInfo : function(sid, ep, callback, format) {
+        if (!format || !(format == 'xml' || format == 'xml2js')) {
+            var realcallback = callback;
+            callback = function(result) {
+                realcallback(clean_episode_info(result));
+            }
+        }
+        ragify(baseUrl + 'episodeinfo.php?sid=' + sid + '&ep=' + ep, callback,
+                format);
+    },
+
+    getEpisodeList : function(id, callback, format) {
+        if (!format || !(format == 'xml' || format == 'xml2js')) {
+            var realcallback = callback;
+            callback = function(result) {
+                realcallback(clean_episode_list(result));
+            }
+        }
+        ragify(baseUrl + 'episode_list.php?sid=' + id, callback, format);
+    },
+
+    getFull : function(id, callback, format) {
+        if (!format || !(format == 'xml' || format == 'xml2js')) {
+            var realcallback = callback;
+            callback = function(result) {
+                realcallback(clean_full(result));
+            }
+        }
+        ragify(baseUrl + 'full_show_info.php?sid=' + id, callback, format);
+    },
+
+    getShowInfo : function(id, callback, format) {
+        if (!format || !(format == 'xml' || format == 'xml2js')) {
+            var realcallback = callback;
+            callback = function(result) {
+                realcallback(clean_show_info(result));
+            }
+        }
+        ragify(baseUrl + 'showinfo.php?sid=' + id, callback, format);
+    },
+
+    listShows : function(callback, format) {
+        if (!format || !(format == 'xml' || format == 'xml2js')) {
+            var realcallback = callback;
+            callback = function(result) {
+                realcallback(clean_list_shows(result));
+            }
+        }
+        ragify(baseUrl + 'show_list.php', callback, format);
+    }
 }
